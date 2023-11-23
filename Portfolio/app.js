40711bdf62ef62e91896c2e8f85fd7676b2384f9
lanwaros <133409579+AnouarXperience@users.noreply.github.com>
2023-11-23 05:21:50 +0100

    Update MyPortfolio

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -201,6 +201,28 @@ const scene5 = new ScrollMagic.Scene({
 .setTween(tlCompetences)
 .addTo(controller);
 
+// Get the button
+var mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
 // This function will be called each time the timeline completes
 // function repeatAnimation() {
     
